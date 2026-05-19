d5a11105a4f541c9a9699548f1a43459a8be60a6
Ryder-CTL <bunnyjryder@gmail.com>
2026-05-19 10:44:42 -0700

    Prompt

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Write your code for Part 1 below
 
 var favColor = prompt("What is your favorite color?");
-allert("Your favorite color is" + favColor "!");
+alert("Your favorite color is" + favColor + "!");
 
 
 // Then, after your tried a prompt yourself,
 // uncomment the two lines below to try Part 2
 
-// var firstName = prompt('What is your name?');
-// alert('Welcome ' + firstName + '!');
+var firstName = prompt('What is your name?');
+alert('Welcome ' + firstName + '!');
